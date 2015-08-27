47465d8178ab5d80951a5f659a29eda964d0433e
Quintin <quintinreineccius@gmail.com>
2015-08-27 11:51:46 -0700

    grabs user input and desplays it on the page

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+//grabbing input field
+var userInput = document.querySelector('.user-input');
 
+//grabbing sumbit button
+var submitButton = document.querySelector('.submit-button');
+
+//making a function that will grab user input and desplay it
+function desplayingUserInput(event){
+  event.preventDefault();
+  var placeHolder = document.querySelector('ul');
+  var userItem = document.createElement('li');
+  var userText = document.createTextNode(userInput.value);
+  userItem.appendChild(userText);
+  placeHolder.appendChild(userItem);
+  userInput.value = "";
+};
+
+submitButton.onclick = desplayingUserInput;
